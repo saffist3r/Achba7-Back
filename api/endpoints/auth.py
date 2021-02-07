51b0c7387668dfdf1c49dd19879316ff59f660e5
Saffist3r <axelsaff@gmail.com>
2021-02-08 00:29:08 +0100

    Updated endpoints.

--- api/endpoints/auth.py
+++ api/endpoints/auth.py
@@ -1,9 +1,7 @@
 from fastapi import APIRouter, Body, Depends, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
 
-
 from starlette.responses import PlainTextResponse, JSONResponse
-from models import logsModel
 router = APIRouter()
 
 
